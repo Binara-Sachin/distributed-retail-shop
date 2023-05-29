577ea40f918347f9cfb9f944acb542a063056e7c
Binara-Sachin <binara.sachin@gmail.com>
2023-05-30 01:25:33 +0530

    Initial Commit

---
+++ client/src/main/java/org/binara/sachin/MainClient.java
@@ -0,0 +1,7 @@
+package org.binara.sachin;
+
+public class MainClient {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ shop-server/src/main/java/org/binara/sachin/ShopServer.java
@@ -0,0 +1,7 @@
+package org.binara.sachin;
+
+public class ShopServer {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
